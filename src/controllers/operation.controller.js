0d524a333648186c565bb753b559c16d9682b754
jirayus <x-mark42@hotmail.com>
2026-04-22 23:03:56 +0700

    feat : add getOperation api

--- src/controllers/operation.controller.js
+++ src/controllers/operation.controller.js
@@ -1,6 +1,13 @@
 const { operationService } = require("../services");
 const { ok, fail, catchAsync } = require("../utils");
 
+exports.getOperation = catchAsync(async (req, res, next) => {
+  const payload = req.body;
+  const result = await operationService.getOperation();
+
+  return ok(res, result);
+});
+
 exports.createOperation = catchAsync(async (req, res, next) => {
   const payload = req.body;
   const result = await operationService.createOperation(payload);
--- src/services/operation.service.js
+++ src/services/operation.service.js
@@ -1,11 +1,17 @@
 const Operation = require("../models/Operation");
 
+const getOperation = async () => {
+  const operation = await Operation.find();
+  return operation ?? [];
+};
+
 const createOperation = async (data) => {
   const operation = await Operation.create(data);
 
-  return operation?._id ?? '';
+  return operation?._id ?? "";
 };
 
 module.exports = {
   createOperation,
+  getOperation,
 };
