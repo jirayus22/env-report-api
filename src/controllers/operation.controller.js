106f211cf6d4c3c8c75aec3eee680319db9007a7
jirayus <x-mark42@hotmail.com>
2026-04-28 00:00:35 +0700

    fix : change condition do value

--- src/controllers/operation.controller.js
+++ src/controllers/operation.controller.js
@@ -25,7 +25,7 @@ const getColor = (field, value) => {
 
   switch (field) {
     case "do":
-      return v > 1 ? "#FF3B30" : "#333333";
+      return v < 1 ? "#FF3B30" : "#333333";
 
     case "sv30":
       return v < 200 || v > 500 ? "#FF3B30" : "#333333";
